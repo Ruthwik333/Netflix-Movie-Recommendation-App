bbefa84e3836807c30273e39556396bef1e1174d
Ruthwik333 <109128609+Ruthwik333@users.noreply.github.com>
2022-07-21 15:40:16 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "[theme]
+primaryColor = ‘salmon’
+backgroundColor = ‘#D3D3D3’
+secondaryBackgroundColor = ‘#FFC6A1’
+textColor= ‘black’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
